69087a87a903256767011b0baecf7dafaf1675f2
khaniel-outis <emirbadoo@gmail.com>
2020-11-03 19:04:51 -0800

    image_picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:pdf_scanner/src/ui/screens/example/example.dart';
 
-List<CameraDescription> cameras;
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  cameras = await availableCameras();
-  runApp(MyApp()); //hello
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/src/ui/screens/example/example.dart
+++ lib/src/ui/screens/example/example.dart
@@ -1,10 +1,6 @@
 import 'dart:io';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-
-import '../../../../main.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ExamplePage extends StatefulWidget {
   @override
@@ -12,54 +8,12 @@ class ExamplePage extends StatefulWidget {
 }
 
 class _ExamplePageState extends State<ExamplePage> {
-  List<File> files = [];
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(),
-  //     body: Center(
-  //       child: ListView(
-  //         children: files
-  //             .map((e) =>
-  //                 Container(height: 200, width: 200, child: Image.file(e)))
-  //             .toList()
-  //             .reversed
-  //             .toList(),
-  //       ),
-  //     ),
-  //     floatingActionButton: FloatingActionButton(onPressed: () async {
-  //       try {
-  //         await ImagePicker()
-  //             .getImage(source: ImageSource.camera)
-  //             .then((value) {
-  //           setState(() {
-  //             files.add(File(value.path));
-  //           });
-  //         });
-  //       } catch (e) {
-  //         print(e.toString());
-  //       }
-  //     }),
-  //   );
-  // }
-  CameraController _cameraController;
+  List<File> _files = [];
+  File _file;
   String error;
   @override
   void initState() {
     super.initState();
-    _cameraController = CameraController(cameras[0], ResolutionPreset.veryHigh);
-    _cameraController.initialize().then((value) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _cameraController?.dispose();
   }
 
   @override
@@ -75,7 +29,7 @@ class _ExamplePageState extends State<ExamplePage> {
             child: ListView(
               shrinkWrap: true,
               scrollDirection: Axis.horizontal,
-              children: files
+              children: _files
                   .map((e) =>
                       Container(height: 200, width: 200, child: Image.file(e)))
                   .toList()
@@ -83,30 +37,18 @@ class _ExamplePageState extends State<ExamplePage> {
                   .toList(),
             ),
           ),
-          Container(
-            height: 200,
-            width: 200,
-            child: CameraPreview(_cameraController),
-          ),
           SizedBox(height: 10),
-          Text(error ?? "Error"),
           TextButton(
               onPressed: () async {
-                try {
-                  final path = join((await getTemporaryDirectory()).path,
-                      '${DateTime.now()}.png');
-                  await _cameraController.takePicture(path);
-                  setState(() {
-                    files.add(File(path));
-                    error = path;
-                  });
-                } catch (e) {
-                  setState(() {
-                    error = e.toString();
-                  });
-                }
+                File _pickedFile = await ImagePicker()
+                    .getImage(source: ImageSource.gallery)
+                    .then((value) => File(value.path));
+
+                _file = _pickedFile;
+                _files.add(File(_file.path));
+                setState(() {});
               },
-              child: Text("Capture"))
+              child: Text("PickImage"))
         ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: null),
